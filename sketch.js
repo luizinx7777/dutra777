90c6948a3e37fdc4231354fa716e12678a3b2030
luizinx7777 <luis.patruceli.santos@escola.pr.gov.br>
2024-09-10 22:21:50 +0000

    gruta

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+      createCanvas(400, 400);
+        background("#673AB7");
+        }
+
+        function draw() {
+          stroke("#8BC34A");
+            fill("#FF9800");
+              
+                   if(mouseIsPressed) {
+                        rect(mouseX, mouseY, 20, 20);
+                          }
+                          }
